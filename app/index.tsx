fc617f072a41028852ddd18469b85c99ec2ab193
kris1027 <krzys89@gmail.com>
2025-03-27 21:57:27 +0100

    customize page header

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function Index() {
+const HomeScreen = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default HomeScreen;
