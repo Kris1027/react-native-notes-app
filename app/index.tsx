5da74043d08d1f41069d1e8379830fd5ee4dc399
kris1027 <krzys89@gmail.com>
2025-03-27 22:32:27 +0100

    add declarations file to fix png files error

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import PostItImage from '@/assets/images/post-it.png';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
+      <Image source={PostItImage} style={styles.image} />
       <Text>Home Screen</Text>
     </View>
   );
@@ -13,6 +15,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
   },
 });
 
